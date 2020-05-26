163d22bdc2b780ba567047717d4db07fa7a0fc26
Mohit Kumar <v-mohkumar@expedia.com>
2020-05-26 09:56:03 +0530

    set up project

---
+++ LinkedList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LinkedList
+//
+//  Created by Mohit Kumar on 26/05/20.
+//  Copyright © 2020 Mohit Kumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
